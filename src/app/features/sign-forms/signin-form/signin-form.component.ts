2f7e397afe1559900d9f74877199b9aca0955c06
Joé FEUCHT <71235356+joe67100@users.noreply.github.com>
2024-05-21 17:15:24 +0200

    feat: signin forms vue

--- src/app/features/sign-forms/signin-form/signin-form.component.ts
+++ src/app/features/sign-forms/signin-form/signin-form.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {CustomInputComponent} from "../../../shared/ui/custom-input/custom-input.component";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signin-form',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    CustomInputComponent,
+    ReactiveFormsModule
+  ],
   templateUrl: './signin-form.component.html',
   styleUrl: './signin-form.component.css'
 })
 export class SigninFormComponent {
 
+  signinForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  })
+
+  onSubmitSignin() {
+    return void 0;
+  }
+
+  get signinInfo() {
+    return this.signinForm.controls;
+  }
 }
--- src/app/shared/ui/custom-input/custom-input.component.ts
+++ src/app/shared/ui/custom-input/custom-input.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-custom-input',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    ReactiveFormsModule
+  ],
   templateUrl: './custom-input.component.html',
   styleUrl: './custom-input.component.css'
 })
 export class CustomInputComponent {
+  @Input() control: FormControl;
+  @Input() placeholder: string;
+  @Input() inputIcon: string;
+  @Input() inputType: string;
 
+  constructor() {
+    this.control = new FormControl();
+    this.placeholder = '';
+    this.inputIcon = '';
+    this.inputType = 'text';
+  }
 }
