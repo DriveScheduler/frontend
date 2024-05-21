dd0104ad9c64f16a3987c2bfee93faefff5c2bc4
Joé FEUCHT <71235356+joe67100@users.noreply.github.com>
2024-05-21 20:12:34 +0200

    feat: signup form

--- src/app/features/sign-forms/signup-form/signup-form.component.ts
+++ src/app/features/sign-forms/signup-form/signup-form.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import {CustomInputComponent} from "../../../shared/ui/custom-input/custom-input.component";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators, FormsModule} from "@angular/forms";
+import {RouterLink} from "@angular/router";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-signup-form',
   standalone: true,
-  imports: [],
+    imports: [
+      CustomInputComponent,
+      ReactiveFormsModule,
+      RouterLink,
+      CommonModule,
+      FormsModule
+    ],
   templateUrl: './signup-form.component.html',
   styleUrl: './signup-form.component.css'
 })
 export class SignupFormComponent {
+  isParticulier: boolean = true;
+  isMoniteur: boolean = false;
 
+  signupForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    drivingSchool: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    passwordConfirmation: new FormControl('', Validators.required),
+    licenseType: new FormControl('', Validators.required),
+  })
+
+  get signupInfo() {
+    return this.signupForm.controls;
+  }
+
+  onSubmitSignup() {
+    return void 0;
+  }
+
+  onCheckboxChange(type: string) {
+    if (type === 'particulier') {
+      this.isParticulier = true;
+      this.isMoniteur = false;
+    } else if (type === 'moniteur') {
+      this.isParticulier = false;
+      this.isMoniteur = true;
+    }
+  }
 }
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,6 +3,5 @@ import { test, expect } from '@playwright/test';
 test('is running', async ({ page }) => {
   await page.goto('http://localhost:4200/');
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/DriveScheduler/);
 });
