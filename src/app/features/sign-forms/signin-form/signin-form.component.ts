df0b461e3bbf2576ea9ce2eaf7f8b7a57b2f94be
Joé FEUCHT <71235356+joe67100@users.noreply.github.com>
2024-05-31 20:04:28 +0200

    prepared for render deployment

--- src/app/features/sign-forms/signin-form/signin-form.component.ts
+++ src/app/features/sign-forms/signin-form/signin-form.component.ts
@@ -3,6 +3,7 @@ import {RouterLink} from "@angular/router";
 import {CustomInputComponent} from "../../../shared/components/custom-input/custom-input.component";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {AuthenticationService} from '../../../shared/services/authentication/authentication.service';
+import { CustomSnackbarService } from "../../../shared/components/custom-snackbar/custom-snackbar.service";
 
 @Component({
   selector: 'app-signin-form',
@@ -16,7 +17,7 @@ import {AuthenticationService} from '../../../shared/services/authentication/aut
   styleUrl: './signin-form.component.css'
 })
 export class SigninFormComponent {
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(private authenticationService: AuthenticationService, private customSnackbar: CustomSnackbarService) {}
 
   signinForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -24,20 +25,32 @@ export class SigninFormComponent {
   })
 
   onSubmitSignin() {
+    if (this.signinForm.invalid) {
+      this.markFormGroupDirty(this.signinForm);
+      this.customSnackbar.show('Veuillez renseigner tous les champs correctement.', 'error');
+      return;
+    }
+
     const email = this.signinForm.value.email!;
     const password = this.signinForm.value.password!;
 
     this.authenticationService.login(email, password).subscribe(
       response => {
-        console.log('User logged in');
+        this.customSnackbar.show('Connexion réussite', 'success')
       },
       error => {
-        console.log('Error logging in');
+        this.customSnackbar.show(error.error, 'error')
       }
     );
   }
 
-  get signinInfo() {
-    return this.signinForm.controls;
+  private markFormGroupDirty(formGroup: FormGroup) {
+    Object.values(formGroup.controls).forEach(control => {
+      control.markAsDirty();
+
+      if (control instanceof FormGroup) {
+        this.markFormGroupDirty(control);
+      }
+    });
   }
 }
