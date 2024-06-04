2f2b60b276f46bee9a1b060554d35575e7f823d6
Joé FEUCHT <71235356+joe67100@users.noreply.github.com>
2024-06-04 10:50:40 +0200

    feat: ce commit n'annonce pas la fin malheureusement

--- src/app/features/administration/administration.component.ts
+++ src/app/features/administration/administration.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {LessonCardComponent} from "src/app/features/dashboard/components/lesson-card/lesson-card.component";
+import {VehicleCardComponent} from "src/app/features/administration/components/vehicle-card/vehicle-card.component";
+import {UserCardComponent} from "src/app/features/administration/components/user-card/user-card.component";
 
 @Component({
   selector: 'app-administration',
   standalone: true,
-  imports: [],
+  imports: [
+    LessonCardComponent,
+    VehicleCardComponent,
+    UserCardComponent
+  ],
   templateUrl: './administration.component.html',
   styleUrl: './administration.component.css'
 })
--- src/app/features/administration/components/vehicle-card/vehicle-card.component.ts
+++ src/app/features/administration/components/vehicle-card/vehicle-card.component.ts
@@ -9,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class VehicleCardComponent {
 
+  deleteVehicle() {
+
+  }
+
+  updateVehicle() {
+
+  }
 }
